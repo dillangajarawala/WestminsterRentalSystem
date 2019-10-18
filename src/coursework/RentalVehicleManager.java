a6a04623317dae30d20f51b9bb3b62ed0340576f
Dillan Gajarawala <dillan.gajarawala@gmail.com>
2019-10-19 00:14:56 +0100

    rental vehicle manager interface created

---
+++ src/coursework/RentalVehicleManager.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coursework;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dillan
+ */
+public interface RentalVehicleManager {
+    
+    public abstract void addVehicle(Vehicle vehicleToAdd);
+    
+    public abstract void deleteVehicle(String plateNumber);
+    
+    public abstract void printVehicles();
+    
+    public abstract void saveVehicleList();
+    
+    public abstract void launchGUI();
+    
+    public abstract ArrayList<Vehicle> filterVehicles(String param, String value);
+    
+    public abstract boolean checkAvailability(String plateNumber, Date start, Date end);
+    
+    public abstract void bookVehicle(String plateNumber, Date pickUpDate, Date dropOffDate);
+}
